176147d2ca4ec15ba5d831cc8ae783a8c6c01dd3
Artur Borowiec <artur.borowiec0@interia.pl>
2022-10-01 14:00:19 +0200

    Add files via upload

---
+++ term1/python-programming/lab1.py
@@ -0,0 +1,54 @@
+# task1
+# name = input("What's your name?\n")
+# print("Hello", name)
+
+# task2
+# name = input("What's your name?")
+# surname = input("What's your surname?")
+# print("Hello ", name, surname)
+
+# task3
+# print("What do you call a bear with no teeth?\nA gummy bear!")
+
+# task4
+# num1 = int(input("Tell me 1st number: "))
+# num2 = int(input("Tell me 2nd number: "))
+# print("The total is", num1+num2)
+
+# task5
+# num1 = int(input("Tell me 1st number: "))
+# num2 = int(input("Tell me 2nd number: "))
+# num3 = int(input("Tell me 3rd number: "))
+# answer = (num1 + num2) * 3
+# print("The answer is", answer)
+
+# task6
+# total = int(input("How many slices of pizza have you started with?\n"))
+# eaten = int(input("How many slices of pizza have you eaten?\n"))
+# print("There are", total-eaten, "slices left")
+
+# task7
+# name = input("What's your name?\n")
+# age = int(input("What's your age?\n"))
+# print(name, "next birthday you will be", age+1)
+
+# task8
+# totalPrice = float(input("What's the total price?\n"))
+# diners = int(input("How many diners are there?\n"))
+# print("Each person needs to pay", "%.2f" % (totalPrice / diners))
+
+# task9
+# days = int(input("Number of days: "))
+# hours = days * 24
+# minutes = hours * 60
+# seconds = minutes * 60
+# print(days, "days are", hours, "hours, ", minutes, "minutes or ", seconds, "seconds")
+
+# task10
+# kilograms = float(input("Enter weight: "))
+# print(kilograms, "kg is", kilograms * 2.204, "pounds")
+
+# task11
+bigNumber = int(input("Enter number over 100: "))
+smallNumber = int(input("Enter number over 100: "))
+print(smallNumber, "can fit", bigNumber//smallNumber, "times in", bigNumber)
