804979490db81e48a4a9846041e6df446d385582
Artur Borowiec <artur.borowiec0@interia.pl>
2022-10-29 13:31:41 +0200

    python-programming: lab3 task 58

--- term1/python-programming/lab3.py
+++ term1/python-programming/lab3.py
@@ -10,7 +10,6 @@ def task56():
     
 def task57():
     num = random.randint(1,10)
-    print(num)
     guess = int(input("Guess a number: "))
     while guess != num:
         if guess > num:
@@ -19,3 +18,16 @@ def task57():
             guess = int(input("Too low, guess again: "))
     print("You guessed correctly")
     
+def task58():
+    score = 0
+    questions_num = 5
+    for i in range(questions_num):
+        num1 = random.randint(1,20)
+        num2 = random.randint(1,20)
+        answer = int(input(f"{num1} + {num2} = "))
+        if answer == num1+num2:
+            print("Right!")
+            score+=1
+        else: print("Wrong!")
+    print(f"You scored {score} points!")
+        
