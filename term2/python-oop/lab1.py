97525294f66fdd4d9b066e18b18f259f4a7d51e9
Artur Borowiec <artur.borowiec0@interia.pl>
2023-05-20 12:15:35 +0200

    python-oop: task1 finished

--- term2/python-oop/lab1.py
+++ term2/python-oop/lab1.py
@@ -5,9 +5,17 @@ class Zespolona:
     def __init__(self, re, im=0.0):
         self.re = re
         self.im = im
+        
+    def __str__(self):
+        return f'{self.re}+{self.im}i'
        
     def modul(self):
         return sqrt(self.re**2 + self.im**2)
     
+    @staticmethod
+    def dodaj(zesp1, zesp2):
+        return Zespolona(zesp1.re+zesp2.re, zesp1.im+zesp2.im)
+    
 z = Zespolona(3, 4)
 print(z.modul())
+print(Zespolona.dodaj(z,z))
