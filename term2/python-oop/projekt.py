4553cfd51fbc59d445a14d6b34269fb7f23a87ba
Artur Borowiec <artur.borowiec0@interia.pl>
2023-06-12 23:41:46 +0200

    python-oop: functions impl

--- term2/python-oop/projekt.py
+++ term2/python-oop/projekt.py
@@ -1,23 +1,32 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
-print("Witaj w bibliotece miejskiej w Sosnowcu!\n")
-print("Wybierz opcje:\n1. dodaj ksiazke")
-print("2. wypozycz ksiazke\n3. oddaj ksiazke")
-print("4. historia ksiazki\n5. zakoncz")
+biblioteka = []
 
-ksiazki = []
+def wypisz_poczatkowe_info():
+    print("Witaj w bibliotece miejskiej w Sosnowcu!\n")
+    print("Wybierz opcje:\n1. dodaj ksiazke")
+    print("2. wypozycz ksiazke\n3. oddaj ksiazke")
+    print("4. historia ksiazki\n5. zakoncz")
 
 def dodaj_ksiazke():
     tytul = input("Tytul ksiazki: ")
     autor = input("Autor ksiazki: ")
     rok_wydania = input("Rok wydania: ")
-    ksiazki.append((tytul, autor, rok_wydania))
-    print(ksiazki)
+    ksiazka = (tytul, autor, rok_wydania, 'w bibliotece')
+    biblioteka.append(ksiazka)
+    print(f"Dodano ksiazke {ksiazka}")
+    print(f"Stan biblioteki: {biblioteka}")
 
-opcja = input("Wybrana opcja: ")
-
-while opcja != '5':
-    match opcja:
-        case '1':
-            dodaj_ksiazke()
+def wypozycz_ksiazke():
+    print('')
+    
+def zacznij_prace_bibliotekarki():
     opcja = input("Wybrana opcja: ")
+    while opcja != '5':
+        match opcja:
+            case '1':
+                dodaj_ksiazke()
+            case '2':
+                wypozycz_ksiazke()
+        opcja = input("Wybrana opcja: ")
+
+wypisz_poczatkowe_info()
+zacznij_prace_bibliotekarki()
