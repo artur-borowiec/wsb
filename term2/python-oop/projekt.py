ec8b42030ab97c1c8629a0814633b208dd6f74bb
Artur Borowiec <artur.borowiec0@interia.pl>
2023-06-23 22:03:14 +0200

    python-oop: project new impl

--- term2/python-oop/projekt.py
+++ term2/python-oop/projekt.py
@@ -1,83 +1,46 @@
-import random
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
 
-biblioteka = []
-historia = []
+ksiazki = []
 czytacze = []
+historia = []
 
-def dodaj_ksiazke():
-    numer = len(biblioteka)
-    tytul = input("Tytul ksiazki: ")
-    autor = input("Autor ksiazki: ")
-    rok_wydania = input("Rok wydania: ")
+class Ksiazka:
+    def __init__(self, id, tytul, autor, rok_wydania, status='W bibliotece'):
+        self.id = id
+        self.tytul = tytul
+        self.autor = autor
+        self.rok_wydania = rok_wydania
+        self.status = status
     
-    ksiazka = (numer, tytul, autor, rok_wydania, 'W bibliotece')
-    biblioteka.append(ksiazka)
-    print(f"Dodano ksiazke {ksiazka}")
-    bstr = str(biblioteka).replace('), (', '),\n(').replace('[', '[\n').replace(']', '\n]')
-    print(f"Stan biblioteki: {bstr}")
+    def __str__(self):
+        return f"{self.id},{self.tytul},{self.autor},{self.rok_wydania},{self.status}"
 
-def dodaj_czytacza(czytacz):
-    czytacze.append(czytacz)
-    print(f"Czytacze: {czytacze}")
+class Czytacz:
+    def __init__(self, id, imie, nazwisko, ilosc_ksiazek=0):
+        self.id = id
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.ilosc_ksiazek = ilosc_ksiazek
     
-def wypozycz_ksiazke():
-    tytul_lub_numer = input("Tytuł lub numer indeksu ksiazki: ")
-    numer_czytacza = input("Numer czytacza: ")
-    imie = input("Imie: ")
-    nazwisko = input("Nazwisko: ")
-    #data = input("Data: ")
-    pozycja = pozycja_ksiazki(tytul_lub_numer)
-    if pozycja == -1:
-        obsluz_bledne_wypozyczenie(None)
-        return
-    
-    ksiazka = biblioteka[pozycja]
-    if (ksiazka[4] == 'Nie w bibliotece'):
-        obsluz_bledne_wypozyczenie(ksiazka)
-        return
+    def __str__(self):
+        return f"{self.id},{self.imie},{self.nazwisko},{self.ilosc_ksiazek}"
         
-    biblioteka[pozycja] = (ksiazka[0], ksiazka[1], ksiazka[2], ksiazka[3], 'Nie w bibliotece')
-    czy_istnieje = czy_czytacz_istnieje(numer_czytacza, imie, nazwisko)
-    
-    if not czy_istnieje:
-        dodaj_czytacza((numer_czytacza, imie, nazwisko, 1))
-    else:
-        zwieksz_ksiazki_czytacza(numer_czytacza)
+class Wypozyczenie:
+    def __init__(self, id, numer_czytacza, czy_udane, data_wyp, data_odd):
+        self.id = id
+        self.numer_czytacza = numer_czytacza
+        self.czy_udane = czy_udane
+        self.data_wyp = data_wyp
+        self.data_odd = data_odd
 
-def obsluz_bledne_wypozyczenie(ksiazka):
-    if ksiazka == None:
-        print("Brak ksiazki")
-    else:
-        print("Ksiazka juz wypozyczona")
-
-def pozycja_ksiazki(tytul_lub_numer):
-    try:
-        numer = int(tytul_lub_numer)
-        numery = [ksiazka[0] for ksiazka in biblioteka]
-        return numery.index(numer) if numer in numery else -1
-    except ValueError:
-        tytul = tytul_lub_numer
-        tytuly = [ksiazka[1] for ksiazka in biblioteka]
-        return tytuly.index(tytul_lub_numer) if tytul in tytuly else -1
-        
-def zwieksz_ksiazki_czytacza(numer):
-    numery = [czytacz[0] for czytacz in czytacze]
-    index = numery.index(numer)
-    czytacz = czytacze[index]
-    czytacze[index] = (czytacz[0], czytacz[1], czytacz[2], 1+czytacz[3])
-    print(f"Czytacze: {czytacze}")
-
-def czy_czytacz_istnieje(numer, imie, nazwisko):
-    numery = [czytacz[0] for czytacz in czytacze]
-    imiona_nazwiska = [(czytacz[1], czytacz[2]) for czytacz in czytacze]
-    print(f"Czytacz z numerem istnieje? {numer in numery}")
-    print(f"Czytacz z imieniem/nazwiskiem istnieje? {(imie, nazwisko) in imiona_nazwiska}")
-    return (numer in numery) or ((imie, nazwisko) in imiona_nazwiska)
-
+    def __str__(self):
+        return f"{self.id},{self.numer_czytacza},{self.czy_udane},{self.data_wyp},{self.data_odd}"
+      
 class AsystentPlikow:
     def __init__(self):
         pass
-        
+    
     def __utworz_plik__(self, nazwa):
         try:
             open(f"{nazwa}", "x")
@@ -89,7 +52,7 @@ class AsystentPlikow:
         self.__utworz_plik__("biblioteka.csv")
         self.__utworz_plik__("historia.csv")
         self.__utworz_plik__("czytacze.csv")
-        
+    
 class Biblioteka:
     def __init__(self, asystent_plikow):
         self.asystent_plikow = asystent_plikow
@@ -103,14 +66,34 @@ class Biblioteka:
     def zacznij_prace(self):
         self.asystent_plikow.sprawdz_pliki()
         self.wypisz_poczatkowe_info()
-        opcja = input("Wybrana opcja: ")
-        while opcja != '5':
-            match opcja:
-                case '1':
-                    dodaj_ksiazke()
-                case '2':
-                    wypozycz_ksiazke()
-            opcja = input("Wybrana opcja: ")
+        
+def wyswietl_kolekcje(kolekcja, nazwa):
+    print(f"==== {nazwa} ====")
+    for element in kolekcja:
+        print(element)
+        
+def zmien_ksiazki_czytacza(numer, zmiana):
+    for cz in czytacze:
+        if cz.id == numer:
+            cz.ilosc_ksiazek += zmiana
+
+def wyswietl_wszystkie_kolekcje():
+    wyswietl_kolekcje(ksiazki, 'Ksiazki')
+    wyswietl_kolekcje(czytacze, 'Czytacze')
+    wyswietl_kolekcje(historia, 'Wypozyczenia')
+
+wyswietl_kolekcje(ksiazki, 'Ksiazki')
+ksiazki.append(Ksiazka(1, "T1", "A1", 2021))
+wyswietl_kolekcje(ksiazki, 'Ksiazki')
+czytacze.append(Czytacz(1, "I1", "N1"))
+czytacze.append(Czytacz(2, "I2", "N1"))
+zmien_ksiazki_czytacza(2, -1)
+wyswietl_kolekcje(czytacze, 'Czytacze')
+historia.append(Wypozyczenie(1, 1, "tak", 21-2-2002, 22-2-2002))
+wyswietl_kolekcje(historia, 'Historia')
+
+wyswietl_wszystkie_kolekcje()
 
 asystent = AsystentPlikow()
-Biblioteka(asystent).zacznij_prace()
+biblioteka = Biblioteka(asystent)
+biblioteka.zacznij_prace()
