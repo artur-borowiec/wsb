83a11ec5eb71ec18754c7ce3ae21815b3b5d2f00
Artur Borowiec <artur.borowiec0@interia.pl>
2023-10-08 12:10:11 +0200

    numeric-methods: lab1

--- term3/numeric_methods/lab1/main.py
+++ term3/numeric_methods/lab1/main.py
@@ -1,6 +1,6 @@
-import string
-from time import time
 import random
+import string
+import time
 
 
 def fib_rec(n):
@@ -20,15 +20,15 @@ def fib_iter(n):
 
 
 def measure_fib_rec(num):
-    start_time = time()
+    start_time = time.time()
     fib_rec(num)
-    print(f'fibRec execution time: {time() - start_time} seconds')
+    print(f'fibRec execution time: {time.time() - start_time} seconds')
 
 
 def measure_fib_iter(num):
-    start_time = time()
+    start_time = time.time()
     fib_iter(num)
-    print(f'fibIter execution time: {time() - start_time} seconds')
+    print(f'fibIter execution time: {time.time() - start_time} seconds')
 
 
 def write_random_letters():
@@ -37,18 +37,14 @@ def write_random_letters():
             textfile.write(f"{random.choice(string.ascii_uppercase)}\n")
 
 
-dane = []
-
-
-def load_data():
+def zad_1_1():
+    dane = []
     file = open("dane0.txt")
     numbers = file.read().split()
     for s in numbers:
         dane.append(int(s))
     file.close()
 
-
-def zad_1_1():
     minimum = 0
     maximum = 0
     for w in range(len(dane)):
@@ -60,5 +56,60 @@ def zad_1_1():
     print(f"max: {maximum}")
 
 
-load_data()
-zad_1_1()
+# load_data()
+# zad_1_1()
+def zad_2():
+    dane = []
+    file = open("dane1.txt")
+    numbers = file.read().split()
+    for s in numbers:
+        dane.append(int(s))
+    file.close()
+
+    start = time.time()
+    selection_sort_2(dane)
+    stop = time.time()
+    print("--- %s sekund ---" % round(stop - start, 2))
+
+
+def bubble_sort(dane):
+    for j in range(0, len(dane)):
+        for i in range(0, len(dane) - 1):
+            if dane[i] > dane[i + 1]:
+                dane[i], dane[i + 1] = dane[i + 1], dane[i]
+
+
+def better_bubble_sort(dane):
+    for j in range(len(dane) - 1, -1, -1):
+        for i in range(0, j):
+            if dane[i] > dane[i + 1]:
+                dane[i], dane[i + 1] = dane[i + 1], dane[i]
+
+
+def selection_sort_2(dane):
+    for i in range(len(dane)):
+        k = i
+        for j in range(i + 1, len(dane)):
+            if dane[k] > dane[j]:
+                k = j
+        dane[i], dane[k] = dane[k], dane[j]
+
+
+def zad3():
+    dane = [-5, -3, 0, 235, 2355, 32153]
+    lb = 0
+    ub = len(dane)-1
+    szukam = 235
+    while lb <= ub:
+        mid = (lb + ub) // 2
+        if dane[mid] == szukam:
+            print("Znalazłem na pozycji: ", mid)
+            break
+        elif dane[mid] > szukam:
+            ub = mid - 1
+        else:
+            lb = mid + 1
+    if lb > ub:
+        print("Nie znalazłem")
+
+zad3()
