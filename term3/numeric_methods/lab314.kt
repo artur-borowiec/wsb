27944153d026c9649d5dd0f6d4a77c343647c0c9
Artur Borowiec <artur.borowiec0@interia.pl>
2023-11-05 11:58:30 +0100

    numeric-methods: lab 3.1.4

---
+++ term3/numeric_methods/lab314.kt
@@ -0,0 +1,12 @@
+// lab 3.1 task 4.
+fun main() {    
+    assert('W'.isUpper())
+}
+
+fun shiftWithKey(input: String, key: String) {
+    
+}
+
+fun Char.isUpper() = this.code in (65..90)
+
+fun Char.isLower() = this.code in (97..122)
