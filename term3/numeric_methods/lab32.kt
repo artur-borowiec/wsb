5066b77bfa5d2e806dc4f1678d3de6a60cfa5ca6
Artur Borowiec <artur.borowiec0@interia.pl>
2023-11-05 09:53:50 +0100

    numeric-methods: lab32

---
+++ term3/numeric_methods/lab32.kt
@@ -0,0 +1,18 @@
+fun main() {    
+    assert(toASCII('C') == 67)
+    assert(toASCII("C") == listOf(67))
+   	assert("!AB*".toBinary() == "10000110000011000010101010")
+}
+
+fun toASCII(input: String): List<Int> =
+    input.toCharArray().map { toASCII(it) }
+
+fun toASCII(input: Char): Int {
+    val output = input.code
+    
+    return output
+}
+
+fun String.toBinary() =
+    toASCII(this).map { Integer.toBinaryString(it) }.joinToString("")
+	
