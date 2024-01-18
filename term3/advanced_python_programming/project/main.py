a742d8fc0648483752b22e95563e01b434285ad5
artur.borowiec <artur.borowiec0@interia.pl>
2024-01-18 21:24:14 +0100

    advanced-python-programming: qr - task 4

--- term3/advanced_python_programming/project/main.py
+++ term3/advanced_python_programming/project/main.py
@@ -1,10 +1,12 @@
 import api
 import cvtool
 import country_info
+import qr
 import map_info
 import number_plates
 import prescription
 import shopping
+
 
 # task2: number_plates
 # number_plates.run()
@@ -12,11 +14,14 @@ import shopping
 # task3: country_info
 # country_info.run()
 
+# task4: qr
+qr.run()
+
 # task5: map_info
 # map_info.run()
 
 # task8: prescription
-prescription.run()
+# prescription.run()
 
 # task9: shopping_list
 # shopping.run()
