a49d1ca8be0c938b1cd2cc6064d8fb81a60abc78
artur.borowiec <artur.borowiec0@interia.pl>
2024-06-16 16:00:23 +0200

    software-automation-tools: weather api testing

--- term4/software-automation-tools/api-testing.py
+++ term4/software-automation-tools/api-testing.py
@@ -1,11 +1,20 @@
 import requests
 
-endpoint = 'http://api.openweathermap.org/data/2.5/weather'
-params = {
-    'q': 'Warsaw',
-    'appid': '',
-    'units': 'metric'
-}
 
-response = requests.get(endpoint, params=params)
-print(response)
+def test_weather_in_city(name):
+    endpoint = 'http://api.openweathermap.org/data/2.5/weather'
+    params = {
+        'q': name,
+        'appid': '23fc47171f1bb26ec537d9bf1a271313',
+        'units': 'metric'
+    }
+
+    response = requests.get(endpoint, params=params)
+
+    assert response.status_code == 200
+    assert response.json()['name'] == name
+    assert response.json()['visibility'] > 0
+
+
+test_weather_in_city('Warsaw')
+test_weather_in_city('New York')
